6f26839c9c1e84b9b9e4a779babc3a154840dbc1
adamcaak <ladamczak1997@gmail.com>
2025-07-02 21:48:16 +0200

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Łukasz Adamczak on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
